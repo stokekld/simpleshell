e6f1f5a28baf271da7205d4b2191e01e7ca4d322
Jesus Flores <joy_warmgun@hotmail.com>
2019-03-08 17:15:12 -0600

    Changed test to add new function  On branch master 	modified:   split_line_test.c

--- src/tests/split_line_test.c
+++ src/tests/split_line_test.c
@@ -1,8 +1,28 @@
 #include <stdio.h>
 
+#include "line.h"
+
 int main()
 {
-    printf("Hello World\n");
+    char *line;
+    char **tokens;
+    int position = 0;
+
+    line = sh_get_line();
+
+    if(!line)
+	return -1;
+
+    tokens = sh_split_line();
+
+    if(!tokens)
+	return -1;
+
+    while(tokens[position] != NULL)
+    {
+	printf("%s\n", tokens[position]);
+	position++;
+    }
 
     return 0;
 }
