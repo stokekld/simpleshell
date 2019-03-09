c56d8568c36a6e68a6c9a01e4ed50d16cf758259
Jesus Flores <joy_warmgun@hotmail.com>
2019-03-08 18:12:25 -0600

    Added function to get tokens od the line  On branch master 	modified:   src/line.c 	modified:   src/line.h

--- src/line.c
+++ src/line.c
@@ -1,4 +1,8 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "line.h"
 
 char *sh_get_line()
 {
@@ -8,4 +12,26 @@ char *sh_get_line()
     getline(&line, &len, stdin);
 
     return line;
+}
+
+char **sh_split_line(char * line)
+{
+    char **tokens = NULL;
+    char *token = NULL;
+    int position = 0;
+
+    tokens = (char **) malloc(TOKEN_BUFSIZE * sizeof(char*));
+
+    token = strtok(line, TOKEN_DELIM);
+
+    while(token != NULL)
+    {
+	tokens[position] = token;
+	token = strtok(NULL, TOKEN_DELIM);
+	position++;
+    }
+
+    tokens[position] = NULL;
+
+    return tokens;
 }
--- src/line.h
+++ src/line.h
@@ -1,1 +1,5 @@
+#define TOKEN_BUFSIZE 10
+#define TOKEN_DELIM " \t\r\n\a"
+
 char *sh_get_line();
+char **sh_split_line(char * line);
