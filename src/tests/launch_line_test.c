a4d03b6546535fe9f7adb0c858ec2df8e20286be
Jesus Flores <joy_warmgun@hotmail.com>
2019-03-14 20:08:59 -0600

    test change for new functions  On branch master 	modified:   src/tests/launch_line_test.c

--- src/tests/launch_line_test.c
+++ src/tests/launch_line_test.c
@@ -1,8 +1,17 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main()
 {
-    printf("Hello world\n");
+    char **line = NULL;
+
+    line = (char **) malloc(3 * sizeof(char *));
+
+    line[0] = "ls";
+    line[1] = "-la";
+    line[2] = NULL;
+
+    sh_launch(line);
 
     return 0;
 }
